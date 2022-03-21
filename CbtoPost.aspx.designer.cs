1a77063fb9393d8ac3fef5ee12f6738c2f718489
Kurami1238 <jacklin1238@gmail.com>
2022-03-21 21:39:02 +0900

    真·無限下拉

--- CbtoPost.aspx.designer.cs
+++ CbtoPost.aspx.designer.cs
@@ -33,6 +33,24 @@ namespace MKForum
         protected global::System.Web.UI.WebControls.Repeater rptStamp;
 
         /// <summary>
+        /// sortid 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden sortid;
+
+        /// <summary>
+        /// hftest 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden hftest;
+
+        /// <summary>
         /// rptcBtoP 控制項。
         /// </summary>
         /// <remarks>
