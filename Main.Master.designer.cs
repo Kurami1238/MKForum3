3b04cd3fe463a1641cd1806b30624162b259800f
SU <bamboo2323231@gmail.com>
2022-03-26 15:56:19 +0800

    母板完善

--- Main.Master.designer.cs
+++ Main.Master.designer.cs
@@ -330,6 +330,24 @@ namespace MKForum
         protected global::System.Web.UI.WebControls.PlaceHolder plhPBDsplMode2;
 
         /// <summary>
+        /// addPBoard 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox addPBoard;
+
+        /// <summary>
+        /// btnAddPB 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddPB;
+
+        /// <summary>
         /// btnPBSave2 控制項。
         /// </summary>
         /// <remarks>
