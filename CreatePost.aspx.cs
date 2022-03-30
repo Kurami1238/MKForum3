931ab1fd1b9c8da4a0e51948e2d824302ca27b31
holeou05fsh <holeou05fsh@gmail.com>
2022-03-30 15:13:04 +0800

    ++

---
+++ CreatePost.aspx.cs
@@ -0,0 +1,154 @@
+using MKForum.Managers;
+using MKForum.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MKForum
+{
+    public partial class CreatePost : System.Web.UI.Page
+    {
+        private PostManager _pmgr = new PostManager();
+        private AccountManager _Amgr = new AccountManager();
+        private Member _member;
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            // 從Session取得登錄者ID
+            this.CheckLogin();
+            // 從Session取得當前子板塊ID
+            int cboardid = this.GetCboardID();
+            // 繫結PostStamp
+            this.BuildPostStamp(cboardid);
+        }
+        private void BuildPostStamp(int cboardid)
+        {
+            List<PostStamp> psList = this._pmgr.GetPostStampList(cboardid);
+            this.dpdlPostStamp.DataSource = psList;
+            this.dpdlPostStamp.DataTextField = "PostSort";
+            this.dpdlPostStamp.DataValueField = "SortID";
+            this.dpdlPostStamp.DataBind();
+        }
+        private int GetCboardID()
+        {
+            int cboardid = 0;
+            if (HttpContext.Current.Session["CboardID"] != null)
+                cboardid = (int)HttpContext.Current.Session["CboardID"];
+            else
+                Response.Redirect($"Index.aspx", true);
+            return cboardid;
+        }
+        private void CheckLogin()
+        {
+            if (this._Amgr.IsLogined())
+            {
+                Member account = this._Amgr.GetCurrentUser();
+                _member = account;
+                this.memberid.Value = this._member.MemberID.ToString();
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            string TitleText = this.txtTitle.Text.Trim();
+            string PostCotentText = this.content.Value.Trim();
+            // 從Session取得當前子板塊ID
+            int cboardid = (int)HttpContext.Current.Session["CboardID"];
+            //檢查必填欄位及關鍵字
+            if (!this._pmgr.CheckInput(TitleText, PostCotentText))
+            {
+                this.lblMsg.Text = this._pmgr.GetmsgText();
+                return;
+            }
+            // 處理類型
+            string postSorttext = this.dpdlPostStamp.Text;
+            int? postSort = Convert.ToInt32(postSorttext);
+            // 處理並儲存圖片
+            Post post = new Post()
+            {
+                Title = TitleText,
+                PostCotent = PostCotentText,
+                SortID = postSort
+            };
+            if (this.fuCoverImage.HasFile)
+            {
+                System.Threading.Thread.Sleep(3);
+                Random random = new Random((int)DateTime.Now.Ticks);
+
+                string folderPath = "~/FileDownload/PostContent/";
+                string fileName = "C" + DateTime.Now.ToString("yyyyMMdd_HHmmss_FFFFFF") + "_" + random.Next(100000).ToString("00000") + Path.GetExtension(this.fuCoverImage.FileName);
+
+                folderPath = HostingEnvironment.MapPath(folderPath);
+                if (!Directory.Exists(folderPath)) // 假如資料夾不存在，先建立
+                    Directory.CreateDirectory(folderPath);
+                string newFilePath = Path.Combine(folderPath, fileName);
+                this.fuCoverImage.SaveAs(newFilePath);
+                post.CoverImage = "/FileDownload/PostContent/" + fileName;
+            }
+            if (post.CoverImage == null)
+            {
+                post.CoverImage = "/FileDownload/PostContent/" + "mokunin.jpg";
+            }
+
+            // 新建一筆Post
+            Guid postid;
+            this._pmgr.CreatePost(this._member.MemberID, cboardid, post, out postid);
+
+            // 處理#tag
+            string htagtext = this.txtPostHashtag.Text;
+            string[] htagarr = htagtext.Split('/');
+            List<string> htaglist = new List<string>();
+            foreach (var x in htagarr)
+            {
+                htaglist.Add(x);
+            }
+            for (int i = 0; i < htaglist.Count; i++)
+            {
+                this._pmgr.CreateHashtag(postid, htaglist[i]);
+            }
+            Response.Redirect($"CbtoPost.aspx?CboardID={cboardid}", true);
+        }
+        protected void btnPostImage_Click(object sender, EventArgs e)
+        {
+            string imgpath = string.Empty;
+            if (this.fuPostImage.HasFile)
+            {
+                System.Threading.Thread.Sleep(3);
+                Random random = new Random((int)DateTime.Now.Ticks);
+
+                string folderPath = "~/FileDownload/PostContent/";
+                string fileName = "P" + DateTime.Now.ToString("yyyyMMdd_HHmmss_FFFFFF")+ "_" + this._member.Account + "_" + random.Next(100000).ToString("00000") + Path.GetExtension(this.fuPostImage.FileName);
+
+                folderPath = HostingEnvironment.MapPath(folderPath);
+                if (!Directory.Exists(folderPath)) // 假如資料夾不存在，先建立
+                    Directory.CreateDirectory(folderPath);
+                string newFilePath = Path.Combine(folderPath, fileName);
+                this.fuPostImage.SaveAs(newFilePath);
+                imgpath = "/FileDownload/PostContent/" + fileName;
+
+                // 儲存圖片路徑
+                    this._pmgr.CreatePostImageList(this._member.MemberID, imgpath);
+                string imagelink = this._pmgr.GetImage(this._member.MemberID);
+                this.content.InnerText += $"![]({imagelink})";
+            }
+        }
+        protected void btnback_Click(object sender, EventArgs e)
+        {
+            this.BackToListPage();
+        }
+        private void BackToListPage()
+        {
+            // 從Session取得當前子板塊ID
+            int cboardid = (int)HttpContext.Current.Session["CboardID"];
+            Response.Redirect($"CbtoPost.aspx?CboardID={cboardid}", true);
+        }
+    }
+} 
---
+++ CreatePost.aspx.designer.cs
@@ -0,0 +1,134 @@
+//------------------------------------------------------------------------------
+// <自動產生的>
+//     這段程式碼是由工具產生的。
+//
+//     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
+//     程式碼，則會遺失變更。
+// </自動產生的>
+//------------------------------------------------------------------------------
+
+namespace MKForum
+{
+
+
+    public partial class CreatePost
+    {
+
+        /// <summary>
+        /// memberid 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputHidden memberid;
+
+        /// <summary>
+        /// txtTitle 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTitle;
+
+        /// <summary>
+        /// dpdlPostStamp 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList dpdlPostStamp;
+
+        /// <summary>
+        /// fuCoverImage 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload fuCoverImage;
+
+        /// <summary>
+        /// imgCoverImage 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgCoverImage;
+
+        /// <summary>
+        /// fuPostImage 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload fuPostImage;
+
+        /// <summary>
+        /// imgPostImage 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPostImage;
+
+        /// <summary>
+        /// btnPostImage 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPostImage;
+
+        /// <summary>
+        /// content 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea content;
+
+        /// <summary>
+        /// txtPostHashtag 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPostHashtag;
+
+        /// <summary>
+        /// lblMsg 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+
+        /// <summary>
+        /// btnSend 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSend;
+
+        /// <summary>
+        /// btnback 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnback;
+    }
+}
