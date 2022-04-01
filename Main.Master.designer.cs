ba04ab1d8aea880f81f3abaf4344839d65548049
SU <bamboo2323231@gmail.com>
2022-04-01 17:52:29 +0800

    格式小改

--- Main.Master.designer.cs
+++ Main.Master.designer.cs
@@ -1,10 +1,10 @@
 ﻿//------------------------------------------------------------------------------
-// <自動產生的>
+// <自動產生>
 //     這段程式碼是由工具產生的。
 //
 //     變更這個檔案可能會導致不正確的行為，而且如果已重新產生
 //     程式碼，則會遺失變更。
-// </自動產生的>
+// </自動產生>
 //------------------------------------------------------------------------------
 
 namespace MKForum
@@ -249,6 +249,15 @@ namespace MKForum
         protected global::System.Web.UI.WebControls.Button btnCBSave1;
 
         /// <summary>
+        /// btnSearh 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSearh;
+
+        /// <summary>
         /// txtSearch 控制項。
         /// </summary>
         /// <remarks>
@@ -265,15 +274,6 @@ namespace MKForum
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList srchDrop;
-
-        /// <summary>
-        /// btnSearh 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSearh;
 
         /// <summary>
         /// btnwebLogin 控制項。
